3c820d15ed3d0fe065e3076bfdae157aca1cdd0c
Kuboczoch <jakub.pokorski@protonmail.com>
2019-08-23 04:58:00 +0200

    #1 #4 #5 Fixing stuff, and rethink design

--- src/comps/App.jsx
+++ src/comps/App.jsx
@@ -1,98 +1,101 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import Map from "./Map";
 
 // Style the app actually
 
 export class App extends Component {
-  constructor() {
-    super();
-    this.handleIss = this.handleIss.bind(this);
-    this.handleCountry = this.handleCountry.bind(this);
-  }
-
-  state = {
-    lat: null,
-    long: null,
-    velocity: null,
-    altitude: null,
-    country: "Fetching Location..."
-  };
-
-  async handleCountry() {
-    const getCountry = await fetch(
-      `https://eu1.locationiq.com/v1/reverse.php?key=8a059ce76eacaf&lat=${
-        this.state.lat
-      }&lon=${this.state.long}&addressdetails=1&format=json`
-    );
-
-    let countryData = await getCountry.json();
-
-    if (countryData.address !== undefined) {
-      this.setState({ country: countryData.address.country });
-    } else {
-      this.setState({ country: null });
+    constructor(props) {
+        super(props);
+        this.handleIss = this.handleIss.bind(this);
+        this.handleCountry = this.handleCountry.bind(this);
     }
 
-    setTimeout(() => {
-      this.handleCountry();
-    }, 10000);
-  }
+    state = {
+        lat: null,
+        long: null,
+        velocity: null,
+        altitude: null,
+        country: "Fetching Location..."
+    };
 
-  async handleIss() {
-    const response = await fetch(
-      "https://api.wheretheiss.at/v1/satellites/25544"
-    );
-    let data = await response.json();
+    async handleCountry() {
+        const getCountry = await fetch(
+            `https://eu1.locationiq.com/v1/reverse.php?key=8a059ce76eacaf&lat=${
+                this.state.lat
+            }&lon=${this.state.long}&addressdetails=1&format=json`
+        );
 
-    this.setState({
-      lat: data.latitude,
-      long: data.longitude,
-      velocity: data.velocity.toFixed(2),
-      altitude: data.altitude.toFixed(2)
-      /* country: countryData.countryName */
-    });
+        let countryData = await getCountry.json();
 
-    setTimeout(() => {
-      this.handleIss();
-    }, 5000);
-  }
+        if (countryData.address !== undefined) {
+            this.setState({country: countryData.address.country});
+        } else {
+            this.setState({country: null});
+        }
 
-  componentDidMount() {
-    this.handleIss();
-    setTimeout(() => {
-      this.handleCountry();
-    }, 1000);
-  }
-
-  render() {
-    let countrycode = "";
-    if (this.state.country === null) {
-      countrycode = <p>The ISS is above ocean.</p>;
-    } else if (this.state.country === "Fetching Location...") {
-      countrycode = <p>{this.state.country}.</p>;
-    } else {
-      countrycode = <p>The ISS is above {this.state.country}.</p>;
+        setTimeout(() => {
+            this.handleCountry();
+        }, 10000);
     }
 
-    if (this.state.lat === null || this.state.long === null) {
-      return <h1>Loading, please wait...</h1>;
-    } else {
-      return (
-        <div className="container">
-          <h1>Current position of ISS (International Space Station)</h1>
-          <div className="details">
-            <h2>ISS Details: </h2>
-            <p>Latitude: {this.state.lat} </p>
-            <p>Longitude: {this.state.long} </p>
-            <p>Altitude: {this.state.altitude} KM </p>
-            <p>Velocity: {this.state.velocity} KM/H </p>
-            {countrycode}
-          </div>
-          <Map className="myMap" lat={this.state.lat} long={this.state.long} />
-        </div>
-      );
+    async handleIss() {
+        const response = await fetch(
+            "https://api.wheretheiss.at/v1/satellites/25544"
+        );
+        let data = await response.json();
+
+        this.setState({
+            lat: data.latitude,
+            long: data.longitude,
+            velocity: data.velocity.toFixed(2),
+            altitude: data.altitude.toFixed(2)
+            /* country: countryData.countryName */
+        });
+
+        setTimeout(() => {
+            this.handleIss();
+        }, 5000);
     }
-  }
+
+    componentDidMount() {
+        this.handleIss();
+        setTimeout(() => {
+            this.handleCountry();
+        }, 1000);
+    }
+
+    render() {
+        let countrycode = "";
+        if (this.state.country === null) {
+            countrycode = <span>The ISS is above ocean.</span>;
+        } else if (this.state.country === "Fetching Location...") {
+            countrycode = <span>{this.state.country}.</span>;
+        } else {
+            countrycode = <span>The ISS is above {this.state.country}.</span>;
+        }
+
+        if (this.state.lat === null || this.state.long === null) {
+            return <h1>Loading, please wait...</h1>;
+        } else {
+            return (
+                <div className="container">
+                    <div className={"header"}>
+                        <div className={"title"}><h1>Current position of ISS</h1>
+                            <span>(International Space Station)</span>
+                        </div>
+                        <ul className="row details">
+                            <li className={"col-sm-3"}>Latitude: {this.state.lat} </li>
+                            <li className={"col-sm-3"}>Longitude: {this.state.long} </li>
+                            <li className={"col-sm-3"}>Altitude: {this.state.altitude} KM</li>
+                            <li className={"col-sm-3"}>Velocity: {this.state.velocity} KM/H</li>
+                            <li className={"col-sm-12"}>{countrycode}</li>
+                        </ul>
+                    </div>
+                    <Map className="myMap" lat={this.state.lat} long={this.state.long}/>
+                </div>
+            );
+        }
+    }
 }
 
 export default App;
--- src/comps/Map.jsx
+++ src/comps/Map.jsx
@@ -1,57 +1,47 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import Leaf from "leaflet";
 
 export class Map extends Component {
-  componentDidMount() {
-    let map = Leaf.map("map", {
-      center: [this.props.lat, this.props.long],
-      zoom: 3
-    });
+    componentDidMount() {
+        let map = Leaf.map("map", {
+            center: [this.props.lat, this.props.long],
+            zoom: 3
+        });
 
-    Leaf.tileLayer(
-      "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibGVuaGF0dGVyIiwiYSI6ImNqemQ0MXN0eTAyeTkzZHF0N2hrYXJ0OXAifQ.B1CbsLP5XDiqxf76mLG0EA",
-      {
-        maxZoom: 3,
-        minZoom: 3,
-        attribution:
-          'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
-          '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-          'Imagery © <a href="http://mapbox.com">Mapbox</a>',
-        id: "mapbox.streets"
-      }
-    ).addTo(map);
+        Leaf.tileLayer(
+            "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibGVuaGF0dGVyIiwiYSI6ImNqemQ0MXN0eTAyeTkzZHF0N2hrYXJ0OXAifQ.B1CbsLP5XDiqxf76mLG0EA",
+            {
+                maxZoom: 3,
+                minZoom: 3,
+                attribution:
+                    'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+                    '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+                    'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+                id: "mapbox.streets"
+            }
+        ).addTo(map);
 
-    let iSS = Leaf.icon({
-      iconUrl:
-        "https://upload.wikimedia.org/wikipedia/commons/d/d0/International_Space_Station.svg",
-      iconSize: [100, 100]
-    });
+        let iSS = Leaf.icon({
+            iconUrl:
+                "https://upload.wikimedia.org/wikipedia/commons/d/d0/International_Space_Station.svg",
+            iconSize: [100, 100]
+        });
 
-    let myISS = Leaf.marker([this.props.lat, this.props.long], {
-      icon: iSS
-    }).addTo(map);
+        let myISS = Leaf.marker([this.props.lat, this.props.long], {
+            icon: iSS
+        }).addTo(map);
 
-    setInterval(() => {
-      let latlng = Leaf.latLng(this.props.lat, this.props.long);
-      myISS.setLatLng(latlng);
-    }, 1000);
-  }
+        setInterval(() => {
+            let latlng = Leaf.latLng(this.props.lat, this.props.long);
+            myISS.setLatLng(latlng);
+        }, 1000);
+    }
 
-  render() {
-    return (
-      <div>
-        <div
-          id="map"
-          style={{
-            height: "50vh",
-            width: "50vw",
-            minWidth: "20rem",
-            textAlign: "center"
-          }}
-        />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div id="map"/>
+        );
+    }
 }
 
 export default Map;
